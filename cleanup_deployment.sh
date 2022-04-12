beaa59c16f5b802fc04226d41921b1b9ce80832d
Ritesh Tarway <rtarway@gmail.com>
2022-04-12 15:35:01 -0500

    Update and rename cleanUpDeployment.sh to cleanup_deployment.sh

--- cleanup_deployment.sh
+++ cleanup_deployment.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-export GKE_CLUSTER_NAME=monetgo-private-cluster
-export PROJECT_ID=my-kubernetes-codelab-345416
-export ZONE=us-central1-c
-export REGION=us-central1
-export ISTIO_HOME=$HOME/istio-1.13.2
-export PATH=$ISTIO_HOME/bin:$PATH
-export NAMESPACE=bookinfoapp
+source ./constants.sh
 
 gcloud container clusters get-credentials $GKE_CLUSTER_NAME --zone $ZONE --project $PROJECT_ID
 
