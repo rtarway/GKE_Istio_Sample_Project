d8bb18705b4fcb2187700bedd888097780855a42
Ritesh Tarway <rtarway@gmail.com>
2022-04-12 14:05:47 -0500

    Update deployReviewsVirtualService.sh
    Added glcoud kluster auth

--- deployReviewsVirtualService.sh
+++ deployReviewsVirtualService.sh
@@ -9,7 +9,7 @@ export NAMESPACE=bookinfoapp
 
 #export NAMESPACE=$(kubectl config view -o jsonpath="{.contexts[?(@.name == \"$(kubectl config current-context)\")].context.namespace}")
 #echo $NAMESPACE
-
+gcloud container clusters get-credentials $GKE_CLUSTER_NAME --zone $ZONE --project $PROJECT_ID
 kubectl config set-context --current --namespace=$NAMESPACE
 kubectl apply -f $ISTIO_HOME/samples/bookinfo/networking/destination-rule-all.yaml
 kubectl apply -f $PWD/admin/virtual-service-reviews-60-20-20.yaml
