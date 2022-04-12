8f06f99976d0a7456c96c401df39530015b9d255
Ritesh Tarway <rtarway@gmail.com>
2022-04-12 15:39:20 -0500

    Update and rename deployIstioGatewayForBookInfo.sh to deploy_bookinfo_gateway.sh

--- deploy_bookinfo_gateway.sh
+++ deploy_bookinfo_gateway.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-export GKE_CLUSTER_NAME=monetgo-private-cluster
-export PROJECT_ID=my-kubernetes-codelab-345416
-export ZONE=us-central1-c
-export REGION=us-central1
-export ISTIO_HOME=$HOME/istio-1.13.2
-export PATH=$ISTIO_HOME/bin:$PATH
-export NAMESPACE=bookinfoapp
+source ./export_constants.sh
 
 #export NAMESPACE=$(kubectl config view -o jsonpath="{.contexts[?(@.name == \"$(kubectl config current-context)\")].context.namespace}")
 #echo $NAMESPACE
