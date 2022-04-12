190616de55a8ddbf03a93d2582b5426b420f75ab
Ritesh Tarway <rtarway@gmail.com>
2022-04-12 15:22:57 -0500

    Update cleanUpDeployment.sh
    Updated to remove destination rule and reviews virtual service

--- cleanUpDeployment.sh
+++ cleanUpDeployment.sh
@@ -12,6 +12,8 @@ gcloud container clusters get-credentials $GKE_CLUSTER_NAME --zone $ZONE --proje
 kubectl config set-context --current --namespace=$NAMESPACE
 
 kubectl delete -f $ISTIO_HOME/samples/bookinfo/networking/bookinfo-gateway.yaml
+kubectl delete -f $ISTIO_HOME/samples/bookinfo/networking/destination-rule-all.yaml
+kubectl delete -f $PWD/admin/virtual-service-reviews-60-20-20.yaml
 kubectl delete -f $ISTIO_HOME/samples/bookinfo/platform/kube/bookinfo.yaml
 
 kubectl config set-context --current --namespace=default
